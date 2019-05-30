6f1dc6fc8e4423022eee3bd48d4f5e48bf8dcfb8
princelewis <princelewis4christ@yahoo.com>
2019-05-30 11:54:59 +0100

    get data from j-son

--- resources/script.js
+++ resources/script.js
@@ -1,4 +1,14 @@
-function hu(){
-    
-    
-}
+$(document).ready(function() {
+    $("form").click(function(){
+        $.get("http://localhost:3000/admin", function(data,status) {
+        let admin = data.find(function(user){
+            user.email === email
+        })
+        var email = $("#email").val();
+        var password = $("#password").val();
+        if (email == admin.email && password == admin.password) {
+                location.replace ("/index-admin.html");
+        }
+        } )
+    })
+})
