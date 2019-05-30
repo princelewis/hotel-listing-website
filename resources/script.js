f36a5c0432907806d0464b929ac1c4b56d156984
princelewis <princelewis4christ@yahoo.com>
2019-05-30 14:13:22 +0100

    create alert for when the login detail is wrong

--- resources/script.js
+++ resources/script.js
@@ -1,14 +1,31 @@
 $(document).ready(function() {
-    $("form").click(function(){
-        $.get("http://localhost:3000/admin", function(data,status) {
-        let admin = data.find(function(user){
-            user.email === email
-        })
+    $("#bt1").click(function(){
         var email = $("#email").val();
         var password = $("#password").val();
-        if (email == admin.email && password == admin.password) {
-                location.replace ("/index-admin.html");
-        }
-        } )
+
+        $.ajax({
+            type: 'GET',
+            url: 'http://localhost:3000/admin',
+            success: function(data) {
+                for(var i=0; i<data.length; i++) {
+                    if (email === data[i].email && password === data[i].password) {
+                        window.location.replace("hotel-listing-website/index-admin.html");
+                        break;
+                    }
+                    else if (email !== data[i].email && password === data[i].password) {
+                        alert("wrong email");
+                        break;
+                    }
+                    else if (email === data[i].email && password !== data[i].password) {
+                        alert("wrong password");
+                        break;
+                    }
+                    else {
+                        alert("wrong details");
+                        break;
+                    }
+                }
+            }
+        })
     })
 })
