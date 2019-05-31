24bec11ba5431f91e772ce6c79ddbb6c1b661750
princelewis <princelewis4christ@yahoo.com>
2019-05-31 09:44:28 +0100

    create user interface

--- resources/script.js
+++ resources/script.js
@@ -1,40 +1,36 @@
-
-$(document).ready(function() {
+$(document).ready(function () {
     $(".hide").hide();
     $.ajax({
-        type:'GET',
+        type: 'GET',
         url: 'http://localhost:3000/hotels',
-        success: function(data){
+        success: function (data) {
             showAllHotels(data);
             fillCity(data);
         }
     })
-    $("#bt1").click(function(){
+    $("#bt1").click(function () {
         var email = $("#email").val();
         var password = $("#password").val();
 
         $.ajax({
             type: 'GET',
             url: 'http://localhost:3000/admin',
-            success: function(data) {
-                for(var i=0; i<data.length; i++) {
+            success: function (data) {
+                for (var i = 0; i < data.length; i++) {
                     if (email === data[i].email && password === data[i].password) {
                         // location.reload();
                         // $("#addbtn").show();
-                         $(".del-hotel").show();
+                        $(".del-hotel").show();
                         // $("#popUp").hide();
-                        window.location.href="index-admin.html";
+                        window.location.href = "index-admin.html";
                         break;
-                    }
-                    else if (email !== data[i].email && password === data[i].password) {
+                    } else if (email !== data[i].email && password === data[i].password) {
                         alert("wrong email");
                         break;
-                    }
-                    else if (email === data[i].email && password !== data[i].password) {
+                    } else if (email === data[i].email && password !== data[i].password) {
                         alert("wrong password");
                         break;
-                    }
-                    else {
+                    } else {
                         alert("wrong details");
                         break;
                     }
@@ -46,7 +42,7 @@ $(document).ready(function() {
 
 
 
-    $("form[name=update]").submit(function(e){
+    $("form[name=update]").submit(function (e) {
         var city = $("#citya").val();
         var hName = $("#hotel-name").val();
         var hLocation = $("#hotel-address").val();
@@ -59,15 +55,15 @@ $(document).ready(function() {
             "name": hName,
             "location": hLocation,
             "price": hPrice,
-            "n-rooms":nRooms,
+            "n-rooms": nRooms,
             "hotel-info": hotelInfo,
             "description": description
         }
         $.ajax({
             type: "POST",
-            url: "http://localhost:3000/"+"hotels",
+            url: "http://localhost:3000/" + "hotels",
             data: hdata,
-            success: function(data) {
+            success: function (data) {
                 alert("Hotel created");
                 // window.location.reload();
 
@@ -78,30 +74,30 @@ $(document).ready(function() {
 
 });
 
-function showAllHotels(data){
+function showAllHotels(data) {
     var container = $(".hotel-container");
     container.empty()
-    if(data.length != 0){
+    if (data.length != 0) {
         var hotelChild = $(".hotel-child");
-        data.forEach(function(hotel){
+        data.forEach(function (hotel) {
             var newHotel = hotelChild.clone();
             newHotel.removeClass("hide");
             newHotel.removeClass("hotel-child");
             var hotelId = hotel["id"];
-            var imgUrl = "resources/images/hotel-"+hotelId+".jpg"
+            var imgUrl = "resources/images/hotel-" + hotelId + ".jpg"
             newHotel.find(".hotl-img").attr("src", imgUrl);
-            newHotel.find(".hotl-name").text(hotel["name"]+" - ");
+            newHotel.find(".hotl-name").text(hotel["name"] + " - ");
             newHotel.find(".hotl-city").text(hotel["city"]);
             var hotelPrice = hotel["price"];
             hotelPrice = hotelPrice.split("-")[1];
-            newHotel.find(".hotl-price").text("₦"+hotelPrice);
+            newHotel.find(".hotl-price").text("₦" + hotelPrice);
             newHotel.find(".hotl-address").text(hotel["location"]);
             var viewButton = newHotel.find(".view-hotel");
             var deleteButton = newHotel.find(".del-hotel");
-            viewButton.click(function(){
+            viewButton.click(function () {
                 displayHotelDetails(hotel);
             });
-            deleteButton.click(function(){
+            deleteButton.click(function () {
                 deleteHotel(hotelId);
             });
             newHotel.appendTo(container).show();
@@ -109,62 +105,62 @@ function showAllHotels(data){
     }
 }
 
-function deleteHotel(hotelId){
+function deleteHotel(hotelId) {
     $.ajax({
-        type:'DELETE',
-        url:'http://localhost:3000/hotels/'+hotelId,
-        success: function() {
+        type: 'DELETE',
+        url: 'http://localhost:3000/hotels/' + hotelId,
+        success: function () {
             location.reload();
-            
+
         }
     })
 }
 
-function displayHotelDetails(hotel){
+function displayHotelDetails(hotel) {
 
-    $(".viewup").on("show.bs.modal", function(){
+    $(".viewup").on("show.bs.modal", function () {
         var hotelId = hotel["id"];
-        var imgUrl = "resources/images/hotel-"+hotelId+".jpg"
+        var imgUrl = "resources/images/hotel-" + hotelId + ".jpg"
         $(".vhotel-img").attr("src", imgUrl);
-        $(".vhotel-name").text(hotel["name"]+" - ");
+        $(".vhotel-name").text(hotel["name"] + " - ");
         $(".vhotel-city").text(hotel["city"]);
         var hotelPrice = hotel["price"];
         hotelPrice = hotelPrice.split("-")[1];
-        $(".vhotel-price").text("₦"+hotelPrice);
+        $(".vhotel-price").text("₦" + hotelPrice);
         $(".vhotel-address").text(hotel["location"]);
     }).modal("show");
-    
+
 }
 
-function fillCity(data){
+function fillCity(data) {
     var cities = new Array();
     var citySelect = $("#select-city");
     citySelect.empty();
     $("<option>").text("Select city").appendTo(citySelect);
-    for(var i = 0; i < data.length; i++){
+    for (var i = 0; i < data.length; i++) {
         var city = data[i]["city"];
         var check = cities.includes(city);
-        if(check){
+        if (check) {
             continue;
-        }else{
+        } else {
             $("<option>").text(city).val(city).appendTo(citySelect);
             cities.push(city);
         }
     }
 }
 
-$("#select-city").change(function(e){
+$("#select-city").change(function (e) {
     var city = $(this).val();
-    if(city === "Select city"){
+    if (city === "Select city") {
 
-    }else{
-        var url = 'http://localhost:3000/hotels/?city='+city;
+    } else {
+        var url = 'http://localhost:3000/hotels/?city=' + city;
         $.ajax({
-                    type: 'GET',
-                    url: url,
-                    success:function(data) { 
-                        showAllHotels(data);
-                    }
-            });
-}
+            type: 'GET',
+            url: url,
+            success: function (data) {
+                showAllHotels(data);
+            }
+        });
+    }
 })
